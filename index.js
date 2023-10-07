9f19f47bacccbfe3eda847b9034f78f7b223eeef
Pankaj <pankajpj3103@gmail.com>
2023-10-08 00:50:32 +0530

    new Ui

--- index.js
+++ index.js
@@ -49,3 +49,38 @@ function populateTable(tableId, data, linkPrefix) {
 populateTable('anime-table', animeData, 'https://www.themoviedb.org/tv/');
 populateTable('movie-table', movieData, 'https://www.themoviedb.org/movie/');
 populateTable('series-table', seriesData, 'https://www.themoviedb.org/tv/');
+
+
+//updating animation of header
+
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
