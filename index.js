a5568b293144b8c37fc3d65e4623ca700571c814
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2023-10-10 03:50:55 +0100

    Implement a descending order toggle

--- index.js
+++ index.js
@@ -16,61 +16,80 @@ function changeCategory(evt, categoryName) {
   evt.currentTarget.className += " active";
 }
 
-document.getElementById("defaultOpen").click();
-
-
-const animeData = [];
-const movieData = [];
-const seriesData = [];
-
-peopleChoiceData.forEach(data => {
-  // anime
-  if (!animeData.find(item => item.id === data.favAnimeID)) {
-    animeData.push({ id: data.favAnimeID, name: data.favAnime, count: 1 });
-  } else {
-    animeData.find(item => item.id === data.favAnimeID).count++;
-  }
-
-  // movie
-  if (!movieData.find(item => item.id === data.favMovieID)) {
-    movieData.push({ id: data.favMovieID, name: data.favMovie, count: 1 });
-  } else {
-    movieData.find(item => item.id === data.favMovieID).count++;
-  }
-
-  // series
-  if (!seriesData.find(item => item.id === data.favSeriesID)) {
-    seriesData.push({ id: data.favSeriesID, name: data.favSeries, count: 1 });
-  } else {
-    seriesData.find(item => item.id === data.favSeriesID).count++;
-  }
-});
-
-// Sort arrays by count
-animeData.sort((a, b) => b.count - a.count);
-movieData.sort((a, b) => b.count - a.count);
-seriesData.sort((a, b) => b.count - a.count);
-
-// animeData = animeData.slice(0, 3);
+function dropTable(tableId) {
+	const table = document.getElementById(tableId);
+	table.innerHTML = '';
+}
 
 // Populate tables
 function populateTable(tableId, data, linkPrefix) {
   const table = document.getElementById(tableId);
   data.forEach(item => {
-    const row = table.insertRow();
-    const nameCell = row.insertCell(0);
-    const countCell = row.insertCell(1);
-    const linkCell = row.insertCell(2);
+	const row = table.insertRow();
+	const nameCell = row.insertCell(0);
+	const countCell = row.insertCell(1);
+	const linkCell = row.insertCell(2);
 
-    nameCell.textContent = item.name;
-    countCell.innerHTML = `<i class="fa fa-heart" style="color: red;"></i> ${item.count}`;
-    linkCell.innerHTML = `<a href="${linkPrefix}${item.id}/" target="_blank"><i class="fa fa-external-link"></i></a>`;
+	nameCell.textContent = item.name;
+	countCell.innerHTML = `<i class="fa fa-heart" style="color: red;"></i> ${item.count}`;
+	linkCell.innerHTML = `<a href="${linkPrefix}${item.id}/" target="_blank"><i class="fa fa-external-link"></i></a>`;
 
   });
 }
-populateTable('anime-table', animeData, 'https://www.themoviedb.org/tv/');
-populateTable('movie-table', movieData, 'https://www.themoviedb.org/movie/');
-populateTable('series-table', seriesData, 'https://www.themoviedb.org/tv/');
+
+function refreshData() {
+	dropTable('anime-table');
+	dropTable('movie-table');
+	dropTable('series-table');
+
+	const animeData = [];
+	const movieData = [];
+	const seriesData = [];
+
+	peopleChoiceData.forEach(data => {
+	  // anime
+	  if (!animeData.find(item => item.id === data.favAnimeID)) {
+		animeData.push({ id: data.favAnimeID, name: data.favAnime, count: 1 });
+	  } else {
+		animeData.find(item => item.id === data.favAnimeID).count++;
+	  }
+
+	  // movie
+	  if (!movieData.find(item => item.id === data.favMovieID)) {
+		movieData.push({ id: data.favMovieID, name: data.favMovie, count: 1 });
+	  } else {
+		movieData.find(item => item.id === data.favMovieID).count++;
+	  }
+
+	  // series
+	  if (!seriesData.find(item => item.id === data.favSeriesID)) {
+		seriesData.push({ id: data.favSeriesID, name: data.favSeries, count: 1 });
+	  } else {
+		seriesData.find(item => item.id === data.favSeriesID).count++;
+	  }
+	});
+
+	// Sort arrays by count
+	if (document.getElementById("descending-checkbox").checked) {
+		animeData.sort((a, b) => b.count - a.count);
+		movieData.sort((a, b) => b.count - a.count);
+		seriesData.sort((a, b) => b.count - a.count);
+	} else {
+		animeData.sort((a, b) => a.count - b.count);
+		movieData.sort((a, b) => a.count - b.count);
+		seriesData.sort((a, b) => a.count - b.count);
+	}
+
+	// animeData = animeData.slice(0, 3);
+
+	populateTable('anime-table', animeData, 'https://www.themoviedb.org/tv/');
+	populateTable('movie-table', movieData, 'https://www.themoviedb.org/movie/');
+	populateTable('series-table', seriesData, 'https://www.themoviedb.org/tv/');
+}
+
+document.getElementById("defaultOpen").click();
+
+refreshData()
 
 document.getElementById('contributor-credits').innerHTML = `${peopleChoiceData.length - 1} contributors`
 
