a5a5ec09a793da806d340d49b326e011cd95a404
nisoojadhav <nisoojadhav@gmail.com>
2023-10-15 23:06:30 +0530

    tr animations

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // changing tabs
 function changeCategory(evt, categoryName) {
-  var i, tabcontent, tablinks;
+  let i, tabcontent, tablinks;
 
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -18,37 +18,36 @@ function changeCategory(evt, categoryName) {
 
 document.getElementById("defaultOpen").click();
 
-
 const animeData = [];
 const movieData = [];
 const seriesData = [];
 
-peopleChoiceData.forEach(data => {
+peopleChoiceData.forEach((data) => {
   // anime
   if (data.favAnime != null && data.favAnimeID != null) {
-	if (!animeData.find(item => item.id === data.favAnimeID)) {
-	  animeData.push({ id: data.favAnimeID, name: data.favAnime, count: 1 });
-	} else {
-	  animeData.find(item => item.id === data.favAnimeID).count++;
-	}
+    if (!animeData.find((item) => item.id === data.favAnimeID)) {
+      animeData.push({ id: data.favAnimeID, name: data.favAnime, count: 1 });
+    } else {
+      animeData.find((item) => item.id === data.favAnimeID).count++;
+    }
   }
 
   // movie
   if (data.favMovie != null && data.favMovieID != null) {
-	if (!movieData.find(item => item.id === data.favMovieID)) {
-	  movieData.push({ id: data.favMovieID, name: data.favMovie, count: 1 });
-	} else {
-	  movieData.find(item => item.id === data.favMovieID).count++;
-	}
+    if (!movieData.find((item) => item.id === data.favMovieID)) {
+      movieData.push({ id: data.favMovieID, name: data.favMovie, count: 1 });
+    } else {
+      movieData.find((item) => item.id === data.favMovieID).count++;
+    }
   }
 
   // series
   if (data.favSeries != null && data.favSeriesID != null) {
-	if (!seriesData.find(item => item.id === data.favSeriesID)) {
-	  seriesData.push({ id: data.favSeriesID, name: data.favSeries, count: 1 });
-	} else {
-	  seriesData.find(item => item.id === data.favSeriesID).count++;
-	}
+    if (!seriesData.find((item) => item.id === data.favSeriesID)) {
+      seriesData.push({ id: data.favSeriesID, name: data.favSeries, count: 1 });
+    } else {
+      seriesData.find((item) => item.id === data.favSeriesID).count++;
+    }
   }
 });
 
@@ -62,8 +61,10 @@ seriesData.sort((a, b) => b.count - a.count);
 // Populate tables
 function populateTable(tableId, data, linkPrefix) {
   const table = document.getElementById(tableId);
-  data.forEach(item => {
+  data.forEach((item) => {
+    console.log(item);
     const row = table.insertRow();
+    row.setAttribute("data-aos", "fade-right");
     const nameCell = row.insertCell(0);
     const countCell = row.insertCell(1);
     const linkCell = row.insertCell(2);
@@ -71,46 +72,55 @@ function populateTable(tableId, data, linkPrefix) {
     nameCell.textContent = item.name;
     countCell.innerHTML = `<i class="fa fa-heart" style="color: red;"></i> ${item.count}`;
     linkCell.innerHTML = `<a href="${linkPrefix}${item.id}/" target="_blank"><i class="fa fa-external-link"></i></a>`;
-
   });
 }
-populateTable('anime-table', animeData, 'https://www.themoviedb.org/tv/');
-populateTable('movie-table', movieData, 'https://www.themoviedb.org/movie/');
-populateTable('series-table', seriesData, 'https://www.themoviedb.org/tv/');
+populateTable("anime-table", animeData, "https://www.themoviedb.org/tv/");
+populateTable("movie-table", movieData, "https://www.themoviedb.org/movie/");
+populateTable("series-table", seriesData, "https://www.themoviedb.org/tv/");
 
-document.getElementById('contributor-credits').innerHTML = `${peopleChoiceData.length - 1} contributors`
+document.getElementById("contributor-credits").innerHTML = `${
+  peopleChoiceData.length - 1
+} contributors`;
 
 // animation of header
-var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\s\\])/g, "<span class='letter'>$&</span>");
+let textWrapper = document.querySelector(".ml11 .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /([^\s\\])/g,
+  "<span class='letter'>$&</span>"
+);
 
-anime.timeline({ loop: true })
+anime
+  .timeline({ loop: true })
   .add({
-    targets: '.ml11 .line',
+    targets: ".ml11 .line",
     scaleY: [0, 1],
     opacity: [0.5, 1],
     easing: "easeOutExpo",
-    duration: 700
+    duration: 700,
   })
   .add({
-    targets: '.ml11 .line',
-    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    targets: ".ml11 .line",
+    translateX: [
+      0,
+      document.querySelector(".ml11 .letters").getBoundingClientRect().width +
+        10,
+    ],
     easing: "easeOutExpo",
     duration: 700,
-    delay: 100
+    delay: 100,
   })
   .add({
-    targets: '.ml11 .letter',
+    targets: ".ml11 .letter",
     opacity: [0, 1],
     easing: "easeOutExpo",
     duration: 600,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i + 1)
+    offset: "-=775",
+    delay: (el, i) => 34 * (i + 1),
   })
   .add({
-    targets: '.ml11',
+    targets: ".ml11",
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
-    delay: 1000
+    delay: 1000,
   });
